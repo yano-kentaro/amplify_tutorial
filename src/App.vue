a823ac8abb97c537148f0aa2610591be578e0d02
yano-kentaro <ynkn0829@gmail.com>
2021-12-26 03:55:26 +0900

    GraphQLを用いたtodoリスト機能実装

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Todo App</h1>
+  <input type="text" v-model="name" placeholder="Todo name">
+  <input type="text" v-model="description" placeholder="Todo description">
+  <button v-on:click="createTodo">Create Todo</button>
+  <div v-for="item in todos" :key="item.id">
+    <h3>{{ item.name }}</h3>
+    <p>{{ item.description }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { API } from 'aws-amplify';
+import { createTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  async created() {
+    this.getTodos();
+  },
+  data() {
+    return {
+      name: '',
+      description: '',
+      todos: []
+    }
+  },
+  methods: {
+    async createTodo() {
+      const { name, description } = this;
+      if (!name || !description) return;
+      const todo = { name, description };
+      await API.graphql({
+        query: createTodo,
+        variables: {input: todo}
+      });
+      this.name = '';
+      this.description = '';
+    },
+    async getTodos() {
+      const todos = await API.graphql({
+        query: listTodos
+      });
+      this.todos = todos.data.listTodos.items;
+    }
   }
 }
 </script>
